34ece41be1d78b5f89e6a7f763d621f701db703a
Karunakar Bandikatla <karunakar.bandikatla@cigna.com>
2017-07-18 20:48:36 +0530

    Added validations.

--- HitList/CoreDataModels/Person+CoreDataProperties.swift
+++ HitList/CoreDataModels/Person+CoreDataProperties.swift
@@ -2,24 +2,25 @@
 //  Person+CoreDataProperties.swift
 //  HitList
 //
-//  Created by Karunakar Bandikatla on 17/07/17.
+//  Created by Karunakar Bandikatla on 14/07/17.
 //  Copyright © 2017 Karunakar Bandikatla. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-extension Person {
 
+extension Person {
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
         return NSFetchRequest<Person>(entityName: "Person");
     }
-
+    
     @NSManaged public var active: Bool
     @NSManaged public var id: Int64
     @NSManaged public var lastUpdated: NSDate?
     @NSManaged public var name: String?
-    @NSManaged public var address: Address?
+    @NSManaged public var address: String?
     @NSManaged public var spouse: Person?
     
     class func entityName() -> String {
@@ -30,5 +31,35 @@ extension Person {
         let predicate: NSPredicate = NSPredicate(format: "active == true")
         return predicate
     }
-
+    
+    func isValidGrade() -> Bool {
+        return false
+    }
+    
+    public override func validateForInsert() throws {
+        if let personName = self.name {
+            if personName.isEmpty {
+                throw NSError(domain: Person.PersonNameErrorDomain, code: Person.errorCodes.minLimitNotReached.rawValue, userInfo: ["message" : Person.PersonNameMinLimit])
+            }
+            else if personName.characters.count > 10 {
+                throw NSError(domain: Person.PersonNameErrorDomain, code: Person.errorCodes.maxLimitExceeded.rawValue, userInfo: ["message" : Person.PersonNameMaxLimit])
+            }
+        }
+        
+        if !self.isValidGrade() {
+            throw NSError(domain: Person.PersonGradeErrorDomain, code: 100, userInfo: ["message" : "Grade should be one of [A, SA, M]"])
+        }
+    }
+    
+    public override func validateForUpdate() throws {
+        if let personName = self.name {
+            if personName.isEmpty {
+                throw NSError(domain: Person.PersonNameErrorDomain, code: Person.errorCodes.minLimitNotReached.rawValue, userInfo: ["message" : Person.PersonNameMinLimit])
+            }
+            else if personName.characters.count > 10 {
+                throw NSError(domain: Person.PersonNameErrorDomain, code: Person.errorCodes.maxLimitExceeded.rawValue, userInfo: ["message" : Person.PersonNameMaxLimit])
+            }
+        }
+    }
+    
 }
